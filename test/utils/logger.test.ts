40507a5d1b0f1ba43c1063adaf709fc465f5f6e2
hydrangea-sh <elizabethkang@fastmail.com>
2024-02-09 11:08:54 -0600

    🐛 fix: linting

--- test/utils/logger.test.ts
+++ test/utils/logger.test.ts
@@ -1,35 +1,34 @@
-
-import { expect, test, mock } from "bun:test";
+import { expect, mock, test } from "bun:test";
 import { log } from "../../src/utils/logger";
 
 globalThis.console = {
-  ...console,
-  error: mock(),
-  log: mock(),
-  info: mock(),
-  warn: mock(),
+	...console,
+	error: mock(),
+	log: mock(),
+	info: mock(),
+	warn: mock(),
 };
 
 test("log.error logs an error message", () => {
-  const testMessage = "Test error message";
-  log.error(testMessage);
-  expect(console.error).toHaveBeenCalledWith(`⛔️ ${testMessage}`);
+	const testMessage = "Test error message";
+	log.error(testMessage);
+	expect(console.error).toHaveBeenCalledWith(`⛔️ ${testMessage}`);
 });
 
 test("log.success logs a success message", () => {
-  const testMessage = "Test success message";
-  log.success(testMessage);
-  expect(console.log).toHaveBeenCalledWith(`✅ ${testMessage}`);
+	const testMessage = "Test success message";
+	log.success(testMessage);
+	expect(console.log).toHaveBeenCalledWith(`✅ ${testMessage}`);
 });
 
 test("log.info logs an info message", () => {
-  const testMessage = "Test info message";
-  log.info(testMessage);
-  expect(console.info).toHaveBeenCalledWith(`️📝 ${testMessage}`);
+	const testMessage = "Test info message";
+	log.info(testMessage);
+	expect(console.info).toHaveBeenCalledWith(`️📝 ${testMessage}`);
 });
 
 test("log.warn logs a warning message", () => {
-  const testMessage = "Test warning message";
-  log.warn(testMessage);
-  expect(console.warn).toHaveBeenCalledWith(`⚠️ ${testMessage}`);
+	const testMessage = "Test warning message";
+	log.warn(testMessage);
+	expect(console.warn).toHaveBeenCalledWith(`⚠️ ${testMessage}`);
 });
