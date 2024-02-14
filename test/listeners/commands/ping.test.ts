ed8d1293ff4e8d1b7a3de285e3bb9570b3d88d8f
hydrangea-sh <elizabethkang@fastmail.com>
2024-02-14 17:52:34 -0600

    📦chore: created boilerplate testing files

---
+++ test/listeners/commands/ping.test.ts
@@ -0,0 +1,21 @@
+import {
+	afterEach,
+	beforeEach,
+	describe,
+	expect,
+	it,
+	mock,
+	test,
+} from "bun:test";
+
+describe("Your Test Suite Name", () => {
+	let bun: string;
+
+	beforeEach(() => {});
+
+	afterEach(() => {});
+
+	it("should do something meaningful", () => {});
+
+	it("should do something else meaningful", () => {});
+});
