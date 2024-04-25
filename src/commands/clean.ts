21d3479fbd0bc44712ce16d455b03985a2ac2daa
hydrangea-sh <elizabethkang@fastmail.com>
2024-04-25 16:52:19 -0500

    📦 chore: disabling unused commands

--- src/commands/clean.ts
+++ src/commands/clean.ts
@@ -1,38 +1,38 @@
-import { SlashCommandBuilder } from "discord.js";
+// import { SlashCommandBuilder } from "discord.js";
 
-export const on = new SlashCommandBuilder()
-	.setName("clean")
-	.setDescription("Clean messages in a channel!")
-	.addIntegerOption((option) =>
-		option
-			.setName("amount")
-			.setDescription("Amount of messages to be deleted")
-			.setRequired(true),
-	);
+// export const on = new SlashCommandBuilder()
+// 	.setName("clean")
+// 	.setDescription("Clean messages in a channel!")
+// 	.addIntegerOption((option) =>
+// 		option
+// 			.setName("amount")
+// 			.setDescription("Amount of messages to be deleted")
+// 			.setRequired(true),
+// 	);
 
-export const action: Action<SlashCommand> = async (interaction) => {
-	const amount = interaction.options.getInteger("amount", true);
+// export const action: Action<SlashCommand> = async (interaction) => {
+// 	const amount = interaction.options.getInteger("amount", true);
 
-	if (!interaction.inGuild()) {
-		await interaction.reply(":x: This command can only be run in a server!");
-		return;
-	}
+// 	if (!interaction.inGuild()) {
+// 		await interaction.reply(":x: This command can only be run in a server!");
+// 		return;
+// 	}
 
-	if (!interaction.channel) {
-		await interaction.reply(
-			":x: This command can only be run in a text channel!",
-		);
-		return;
-	}
+// 	if (!interaction.channel) {
+// 		await interaction.reply(
+// 			":x: This command can only be run in a text channel!",
+// 		);
+// 		return;
+// 	}
 
-	if (amount > 100) {
-		await interaction.editReply(
-			":x: You can't delete more than 100 messages at a time!",
-		);
-		return;
-	}
+// 	if (amount > 100) {
+// 		await interaction.editReply(
+// 			":x: You can't delete more than 100 messages at a time!",
+// 		);
+// 		return;
+// 	}
 
-	const { size } = await interaction.channel.bulkDelete(amount, true);
+// 	const { size } = await interaction.channel.bulkDelete(amount, true);
 
-	await interaction.reply(`Successfully deleted **${size}** messages`);
-};
+// 	await interaction.reply(`Successfully deleted **${size}** messages`);
+// };
--- src/commands/inspire.ts
+++ src/commands/inspire.ts
@@ -1,25 +1,25 @@
-import { SlashCommandBuilder } from "discord.js";
-import { log } from "../utils/logger";
+// import { SlashCommandBuilder } from "discord.js";
+// import { log } from "../utils/logger";
 
-interface Quote {
-	content: string;
-	author: string;
-}
+// interface Quote {
+// 	content: string;
+// 	author: string;
+// }
 
-export const on = new SlashCommandBuilder()
-	.setName("inspire")
-	.setDescription("Returns a random inspirational quote");
+// export const on = new SlashCommandBuilder()
+// 	.setName("inspire")
+// 	.setDescription("Returns a random inspirational quote");
 
-export const action: Action<SlashCommand> = async (interaction) => {
-	try {
-		const res = await fetch("https://api.quotable.io/random");
-		const { content, author } = await res.json<Quote>();
-		await interaction.reply(`"${content}"  - ${author}`);
-	} catch (error) {
-		await interaction.reply(":x: There was an error fetching quote");
-		log.error(
-			"There was an issue fetching quote, json-ing object, or replying to message",
-			error,
-		);
-	}
-};
+// export const action: Action<SlashCommand> = async (interaction) => {
+// 	try {
+// 		const res = await fetch("https://api.quotable.io/random");
+// 		const { content, author } = await res.json<Quote>();
+// 		await interaction.reply(`"${content}"  - ${author}`);
+// 	} catch (error) {
+// 		await interaction.reply(":x: There was an error fetching quote");
+// 		log.error(
+// 			"There was an issue fetching quote, json-ing object, or replying to message",
+// 			error,
+// 		);
+// 	}
+// };
