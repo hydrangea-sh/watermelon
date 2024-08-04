13974eb8c5f0607eca79d96ae762a34f94c22fbd
hydrangea-sh <elizabethkang@fastmail.com>
2024-08-03 21:53:19 -0500

    📦 chore: linting
    Signed-off-by: hydrangea-sh <elizabethkang@fastmail.com>

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,31 @@ import { setListeners } from "./utils/listeners";
 import { log } from "./utils/logger";
 
 async function startDiscordBot() {
-  const client = new Client({
-    intents: [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.MessageContent,
-    ],
-  });
+	const client = new Client({
+		intents: [
+			GatewayIntentBits.Guilds,
+			GatewayIntentBits.GuildMessages,
+			GatewayIntentBits.MessageContent,
+		],
+	});
 
-  try {
-    log.info("Discord bot is starting.");
-    await setListeners(client);
-    await client.login(Bun.env.DISCORD_TOKEN);
-    log.success("Discord bot is successfully logged in and initialized.");
-  } catch (error) {
-    log.error("Error initializing Discord bot:", error);
-    throw error;
-  }
+	try {
+		log.info("Discord bot is starting.");
+		await setListeners(client);
+		await client.login(Bun.env.DISCORD_TOKEN);
+		log.success("Discord bot is successfully logged in and initialized.");
+	} catch (error) {
+		log.error("Error initializing Discord bot:", error);
+		throw error;
+	}
 }
 
 async function main() {
-  try {
-    await startDiscordBot();
-  } catch (error) {
-    log.error("An error occurred during initialization:", error);
-  }
+	try {
+		await startDiscordBot();
+	} catch (error) {
+		log.error("An error occurred during initialization:", error);
+	}
 }
 
 main();
