be1928e8d64a74f7959f9ac05b1a48505829bc27
hydrangea-sh <elizabethkang@fastmail.com>
2024-09-04 21:21:34 -0500

    📦 chore: linting
    Signed-off-by: hydrangea-sh <elizabethkang@fastmail.com>

--- src/commands/embed.ts
+++ src/commands/embed.ts
@@ -5,17 +5,14 @@ export const on = new SlashCommandBuilder()
 	.setName("embed")
 	.setDescription("Post a social media url to a nice embed")
 	.addStringOption((option) =>
-		option
-			.setName("url")
-			.setDescription("Social media url")
-			.setRequired(true),
+		option.setName("url").setDescription("Social media url").setRequired(true),
 	);
 
 export const action: Action<SlashCommand> = async (interaction) => {
 	const reply = await interaction.deferReply({ fetchReply: true });
-	
+
 	try {
-		const url = interaction.options.getString("url", true)
+		const url = interaction.options.getString("url", true);
 
 		if (!containsValidUrl(url)) {
 			await interaction.followUp("Please have a valid url.");
@@ -25,7 +22,6 @@ export const action: Action<SlashCommand> = async (interaction) => {
 		await interaction.followUp({
 			content: `${replacer(url)}`,
 		});
-
 	} catch (error) {
 		console.error("Error embedding URL:", error);
 		await interaction.followUp("An error occurred while created the url.");
